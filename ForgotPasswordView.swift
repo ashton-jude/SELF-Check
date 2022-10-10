a999fd5c8f6a993bbceea30dd6299616c84a3e1b
Ashton Reddy <40435245+ashton-reddy@users.noreply.github.com>
2022-10-10 12:59:17 -0400

    refine and added screens

---
+++ ForgotPasswordView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ForgotPasswordView: View {
+    var body: some View {
+        Text("Forgot Password")
+    }
+}
+
+struct ForgotPasswordView_Previews: PreviewProvider {
+    static var previews: some View {
+        ForgotPasswordView()
+    }
+}
--- LoginView.swift
+++ LoginView.swift
@@ -8,35 +8,55 @@ struct LoginView: View {
     
     var body: some View {
         NavigationView{
-        VStack(alignment: .leading, spacing: 16) {
-            Text("Email")
-                .font(.callout).bold()
-            TextField("example@example.com", text: $email)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            Text("Password")
-                .font(.callout).bold()
-            SecureField("password", text: $password)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            
-                
-                NavigationLink(  destination: CheckinView() , isActive: self.$active) { 
-                    Text("Sign In")
+            ZStack(alignment: .center) { 
+                VStack{
+                    Text("Welcome to S.E.L.F Check")
+                        .font(.title)
+                        .fontWeight(.bold)
                     Spacer()
+                VStack(spacing: 16) {
+                    VStack(alignment: .leading, spacing: 5 ){ 
+                        Text("Email")
+                            .font(.callout).bold()
+                        TextField("example@example.com", text: $email)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                    }
+                    VStack(alignment: .leading, spacing: 5) { 
+                        Text("Password")
+                            .font(.callout).bold()
+                        SecureField("password", text: $password)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                    }
                     
+                    NavigationLink(  destination: CheckinView() , isActive: self.$active) { 
+                        Text("Forgot your password? Reset it now!")
+                            .font(.subheadline)
+                            .foregroundColor(Color.black)
+                    }
                 }
+                .frame(width: 250)
+                    Spacer()
+                    NavigationLink(  destination: CheckinView() , isActive: self.$active) { 
+                        Text("Admin Login")
+                            .frame(width: 250, height: 40)
+                            .background(Color.white)
+                            .cornerRadius(8)
+                            .foregroundColor(Color.black)
+                    }
+                    NavigationLink(  destination: CheckinView() , isActive: self.$active) { 
+                        Text("Student Login")
+                            .frame(width: 250, height: 40)
+                            .background(Color.white)
+                            .cornerRadius(8)
+                            .foregroundColor(Color.black)
+                    }
+            }
+                .padding(.vertical)
                 
-                
-                
-            
-            .padding()
-            .background(Color(UIColor.systemOrange))
-            .cornerRadius(15)
-            .foregroundColor(Color.white)
+            }
+            .background(Image("Orange").ignoresSafeArea())
         }
-            .padding()
-            .background(Color(UIColor.systemGray6))
-            .cornerRadius(20)
-        }
+        .navigationViewStyle(.stack)
     }
 }
 
